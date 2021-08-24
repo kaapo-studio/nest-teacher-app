317818e3bf6bc91ddbf7b1750951090fad15dc48
Kaapo.Studio <contact@kaapo.studio>
2021-08-24 16:35:21 +0300

    Nest-Teacher-App: Data transfer object done;

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,24 +1,35 @@
 import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  CreateStudentDto,
+  FindStudentResponseDto,
+  StudentResponseDto,
+  UpdateStudentDto,
+} from './dto/student.dto';
 
 @Controller('students')
 export class StudentController {
   @Get()
-  getStudents() {
+  getStudents(): FindStudentResponseDto[] {
     return 'All students';
   }
 
   @Get('/:studentId')
-  getStudentById(@Param('studentId') studentId: string) {
+  getStudentById(
+    @Param('studentId') studentId: string,
+  ): FindStudentResponseDto {
     return `Get Student with Id ${studentId}`;
   }
 
   @Post()
-  createStudent(@Body() body) {
+  createStudent(@Body() body: CreateStudentDto): StudentResponseDto {
     return `Create Student With the Following data ${JSON.stringify(body)}`;
   }
 
   @Put('/:studentId')
-  updateStudent(@Param('studentId') studentId: string, @Body() body) {
+  updateStudent(
+    @Param('studentId') studentId: string,
+    @Body() body: UpdateStudentDto,
+  ): StudentResponseDto {
     return `Update Student with id of ${studentId} With data of ${JSON.stringify(
       body,
     )}`;
